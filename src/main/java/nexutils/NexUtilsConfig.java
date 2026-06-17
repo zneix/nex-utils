44814a10922049930b23b1569c2f998f03d305e0
zneix <zneix@zneix.eu>
2026-06-17 21:15:55 +0200

    Added more entrance prevention logic
    There's new settings added that prevent entering the arena with:
    - Chugging barrel in player's inventory
    - Saturated or Imbued heart in player's inventory
    - Lack of Book of the dead in player's inventory
    
    decided to move away from declaring `menuEntryColor` as separate helper
    variables, since there's few more text options for this, which are just
    declared within `onPostMenuSort` event's logic

--- src/main/java/nexutils/NexUtilsConfig.java
+++ src/main/java/nexutils/NexUtilsConfig.java
@@ -8,10 +8,16 @@ import net.runelite.client.config.ConfigSection;
 @ConfigGroup(NexUtilsConfig.GROUP_KEY)
 public interface NexUtilsConfig extends Config {
 	String GROUP_KEY = "nex-utils";
+
 	String ALTAR_LEFT_CLICK_TP = "altar-left-click-tp";
 	String ALTAR_PREVENT_NO_ZAROS_ITEM = "altar-prevent-no-zaros-item";
-	String MUTE_BLOOD_REAVERS = "mute-blood-reavers";
+
 	String ENTRANCE_PREVENT_EMPTY_INV = "entrance-prevent-empty-inv";
+	String ENTRANCE_PREVENT_CHUGGING_BARREL = "entrance-prevent-chugging-barrel";
+	String ENTRANCE_PREVENT_SATURATED_HEART = "entrance-prevent-saturated-heart";
+	String ENTRANCE_PREVENT_NO_BOTD = "entrance-prevent-no-botd";
+
+	String MUTE_BLOOD_REAVERS = "mute-blood-reavers";
 	String REOPEN_CHAT_ON_FINISH = "reopen-chat-on-finish";
 
 	@ConfigSection(
@@ -21,6 +27,14 @@ public interface NexUtilsConfig extends Config {
 		closedByDefault = false
 	)
 	String altarSection = "Altar";
+
+	@ConfigSection(
+		name = "Entrance prevention",
+		description = "Changes helping to avoid entering with wrong inventory",
+		position = 5,
+		closedByDefault = false
+	)
+	String entrancePreventSection = "Entrance prevention";
 
 	@ConfigSection(
 		name = "Miscellaneous",
@@ -54,6 +68,54 @@ public interface NexUtilsConfig extends Config {
 	}
 
 	@ConfigItem(
+		keyName = ENTRANCE_PREVENT_EMPTY_INV,
+		name = "Non-full inventory",
+		description = "Deprioritizes usage of entrance barrier to Nex arena when your inventory isn't completely full.<br>"
+			+ "Helps if you accidentally don't withdraw e.g. food items after pre-potting.",
+		position = 0,
+		section = entrancePreventSection
+	)
+	default boolean entrancePreventEmptyInv() {
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = ENTRANCE_PREVENT_CHUGGING_BARREL,
+		name = "Chugging barrel",
+		description = "Deprioritizes usage of entrance barrier to Nex arena when<br>"
+			+ "Chugging barrel is present in your inventory.",
+		position = 10,
+		section = entrancePreventSection
+	)
+	default boolean entrancePreventChuggingBarrel() {
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = ENTRANCE_PREVENT_SATURATED_HEART,
+		name = "Saturated/Imbued heart",
+		description = "Deprioritizes usage of entrance barrier to Nex arena when<br>"
+			+ "Saturated or Imbued heart is present in your inventory.",
+		position = 20,
+		section = entrancePreventSection
+	)
+	default boolean entrancePreventSaturatedHeart() {
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = ENTRANCE_PREVENT_NO_BOTD,
+		name = "No Book of the dead",
+		description = "Deprioritizes usage of entrance barrier to Nex arena when<br>"
+			+ "Book of the dead is NOT present in your inventory.",
+		position = 30,
+		section = entrancePreventSection
+	)
+	default boolean entrancePreventNoBOTD() {
+		return true;
+	}
+
+	@ConfigItem(
 		keyName = MUTE_BLOOD_REAVERS,
 		name = "Mute Blood Reavers in kc room",
 		description = "Mutes Blood Reaver attacks in the minion room before bank.",
@@ -62,18 +124,6 @@ public interface NexUtilsConfig extends Config {
 	)
 	default boolean muteBloodReavers() {
 		return false;
-	}
-
-	@ConfigItem(
-		keyName = ENTRANCE_PREVENT_EMPTY_INV,
-		name = "Prevent entry when inv isn't full",
-		description = "Deprioritizes usage of entrance barrier to Nex arena when inventory isn't completely full.<br>"
-			+ "Helps if you accidentally don't withdraw e.g. food items after pre-potting.",
-		position = 10,
-		section = miscSection
-	)
-	default boolean entrancePreventEmptyInv() {
-		return true;
 	}
 
 	@ConfigItem(
